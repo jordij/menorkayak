b67fdef461dce5b810d64f2e99b0a80098d6a221
jordij <jordi.joan@gmail.com>
2017-04-20 22:38:23 +1200

    Cache settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -198,6 +198,14 @@ LOGGING = {
     }
 }
 
+MIDDLEWARE_CLASSES += (
+   'django.middleware.cache.UpdateCacheMiddleware',
+   'django.middleware.cache.FetchFromCacheMiddleware',
+)
+CACHE_MIDDLEWARE_ALIAS = 'mk'
+CACHE_MIDDLEWARE_SECONDS = 31449600  # 1 year
+CACHE_MIDDLEWARE_KEY_PREFIX = CACHE_MIDDLEWARE_ALIAS
+
 # Custom Admin URL, use {% url 'admin:index' %}
 ADMIN_URL = env('DJANGO_ADMIN_URL')
 BASE_URL = 'https://menorkayak.com'
